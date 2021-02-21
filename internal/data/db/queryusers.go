32cabedf9bff1558e1024f7b774a44b570acb75f
vitaminwater <stantin04@gmail.com>
2021-02-21 12:14:02 +0100

    wip - notification system for comments and alerts

---
+++ internal/data/db/queryusers.go
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2021  SuperGreenLab <towelie@supergreenlab.com>
+ * Author: Constantin Clauzel <constantin.clauzel@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package db
+
+import (
+	"github.com/gofrs/uuid"
+	"github.com/sirupsen/logrus"
+	"upper.io/db.v3/postgresql"
+)
+
+func GetUser(userID uuid.UUID) (User, error) {
+	user := User{}
+
+	var err error
+	sess, err := postgresql.Open(Settings)
+	if err != nil {
+		logrus.Errorf("db.Open(): %q\n", err)
+		return user, err
+	}
+	defer sess.Close()
+
+	selector := sess.Select("*").From("users")
+	if err = selector.One(&user); err != nil {
+		logrus.Error(err.Error())
+		return user, err
+	}
+
+	return user, nil
+}
